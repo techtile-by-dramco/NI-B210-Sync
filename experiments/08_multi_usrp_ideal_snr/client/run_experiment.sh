94cae003d4a72d78f75b66a19264cc8772a54669
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2025-10-23 18:02:50 +0200

    Update run_experiment.sh

--- experiments/08_multi_usrp_ideal_snr/client/run_experiment.sh
+++ experiments/08_multi_usrp_ideal_snr/client/run_experiment.sh
@@ -19,7 +19,8 @@ MEAS_ID=1
 HOST_T04 = "192.108.1.82"
 
 
-for ((GAIN_TX=GAIN_TX_START; GAIN_TX<=GAIN_TX_STOP; GAIN_TX+=GAIN_STEP)); do
+for ((GAIN_TX=GAIN_TX_START; GAIN_TX<=GAIN_TX_STOP; GAIN_TX+=GAIN_STEP))
+    do
     echo "Running with TX gain = $GAIN_TX"
     ssh pi@$HOST_T04 "python3 examples/tx_waveforms.py \
         --args 'type=b200' \
@@ -30,7 +31,7 @@ for ((GAIN_TX=GAIN_TX_START; GAIN_TX<=GAIN_TX_STOP; GAIN_TX+=GAIN_STEP)); do
         --wave-freq 0e5 \
         --wave-ampl 0.8 \
         --gain $GAIN_TX"
-done
+    done
 
 # for ((i=1; i<=ITERATIONS; i++))
 #     do
