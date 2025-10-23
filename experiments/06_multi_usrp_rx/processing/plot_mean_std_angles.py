5feeb9347ee48fc923a01d0a13818f7682d57bdd
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2025-10-23 16:08:59 +0200

    add phase_difference plot 06

--- experiments/06_multi_usrp_rx/processing/plot_mean_std_angles.py
+++ experiments/06_multi_usrp_rx/processing/plot_mean_std_angles.py
@@ -2,9 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 import matplotlib.colors as mcolors
+import os
 
 # Function to plot the phase difference vs RX gain B with mean and variance
-def plot_phase_difference_vs_gain(csv_file):
+def plot_phase_difference_vs_gain(csv_file, save_path):
     # Load the CSV file containing circmean and circstd
     data = pd.read_csv(csv_file)
 
@@ -59,7 +60,8 @@ def plot_phase_difference_vs_gain(csv_file):
     
     # Save the figure
     plot_filename = 'phase_difference_vs_gainB_with_variance.png'
-    plt.savefig(plot_filename)
+    # plt.savefig(plot_filename) OLD WAY
+    plt.savefig(os.path.join(save_path, plot_filename))
     plt.show()
     plt.close()
     
@@ -72,8 +74,11 @@ def main():
                         help="Path to the CSV file with circmean and circstd values (default: './results/circmean_and_circstd.csv')")
     args = parser.parse_args()
 
+    # Define the directory path where the generated figure will be saved
+    save_path = os.path.dirname(os.path.dirname(args.csv_file))
+
     # Plot phase difference vs RX gain B with circular variance
-    plot_phase_difference_vs_gain(args.csv_file)
+    plot_phase_difference_vs_gain(args.csv_file, save_path)
 
 if __name__ == "__main__":
     main()
