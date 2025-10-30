168105a73f681268c090bb6c943c672e808cb90b
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2025-10-30 15:26:28 +0100

    Results 20

--- experiments/20_loopback_single_b210/processing/plot_mean_std_angles.py
+++ experiments/20_loopback_single_b210/processing/plot_mean_std_angles.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
+import os
+import re
 
 # Function to plot the phase difference vs RX gain B with mean and variance
-def plot_phase_difference_vs_gain(csv_file):
+def plot_phase_difference_vs_gain(csv_file, save_path):
     # Load the CSV file containing circmean and circstd
     data = pd.read_csv(csv_file)
 
     # Extract the values
-    gain_b_values = data['RX Gain B']
+    gain_b_values = data['Meas id']
     circ_mean_deg = data['Circular Mean (degrees)']
     circ_std_deg = data['Circular Std Dev (degrees)']
 
@@ -20,15 +22,16 @@ def plot_phase_difference_vs_gain(csv_file):
     plt.errorbar(gain_b_values, circ_mean_deg, yerr=circ_std_deg, fmt='o', color='b', label='Variance')
 
     # Format and display the plot
-    plt.title('Phase Difference vs RX Gain B with Circular Variance (Mean over all files)')
-    plt.xlabel('RX Gain B')
+    plt.title('Phase Difference vs Time with Circular Variance (Mean over all files)')
+    plt.xlabel('Measurement ID')
     plt.ylabel('Average Phase Difference (Degrees)')
     plt.grid(True)
     plt.legend()
     
     # Save the figure
-    plot_filename = 'phase_difference_vs_gainB_with_variance.png'
-    plt.savefig(plot_filename)
+    plot_filename = 'phase_difference_vs_time_with_variance.png'
+    # plt.savefig(plot_filename) OLD WAY
+    plt.savefig(os.path.join(save_path, plot_filename))
     plt.show()
     plt.close()
     
@@ -41,8 +44,16 @@ def main():
                         help="Path to the CSV file with circmean and circstd values (default: './results/circmean_and_circstd.csv')")
     args = parser.parse_args()
 
+    # Define the directory path where the generated figure will be saved
+    save_path = os.path.dirname(os.path.dirname(args.csv_file))
+
+    # Search for tile name
+    match = re.search(r'([A-Za-z]\d{2})_', args.csv_file)
+    if match:
+        tile_name = match.group(0).strip('_')
+
     # Plot phase difference vs RX gain B with circular variance
-    plot_phase_difference_vs_gain(args.csv_file)
+    plot_phase_difference_vs_gain(args.csv_file, save_path)
 
 if __name__ == "__main__":
     main()
