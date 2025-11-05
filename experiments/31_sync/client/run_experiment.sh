a8093e1ec16f14cbce04934b002ccc07ff671ba6
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2025-11-05 11:15:21 +0100

    Update run_experiment.sh

--- experiments/31_sync/client/run_experiment.sh
+++ experiments/31_sync/client/run_experiment.sh
@@ -6,5 +6,5 @@ ITERATIONS=1       # Number of iterations per gain value
 for ((i=1; i<=ITERATIONS; i++))
     do
         echo "Running iteration $i"
-        python3 ursp-cal-bf.py
+        python3 usrp-cal-bf.py
 done
