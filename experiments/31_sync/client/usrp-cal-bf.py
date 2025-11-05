c9683dd4822db0ec8a116ec716da54a79b72616a
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2025-11-05 09:53:10 +0100

    Update usrp-cal-bf.py

--- experiments/31_sync/client/usrp-cal-bf.py
+++ experiments/31_sync/client/usrp-cal-bf.py
@@ -896,7 +896,7 @@ def main():
             quit_event,
             phase_corr=phi_LB + phi_P + np.deg2rad(phi_cable),
             at_time=start_next_cmd,
-            long_time=True,
+            long_time=False, # Set long_time True if you want to transmit longer than 10 seconds
         )
 
         print("DONE")
