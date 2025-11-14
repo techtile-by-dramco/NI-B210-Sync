8ffd7ec378ee39b8fbee0303e766d8ad23629907
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2025-11-14 13:31:57 +0100

    update

--- experiments/30_mulit_usrp_sync_only_lb/processing/plot-phases-same-splitter.py
+++ experiments/30_mulit_usrp_sync_only_lb/processing/plot-phases-same-splitter.py
@@ -15,21 +15,18 @@ df = pd.read_csv(output_path)
 df['ts'] = pd.to_datetime(df['ts'], format='%Y%m%d_%H%M%S')
 
 plt.figure(figsize=(10,6))
-# plt.plot(df['ts'], abs(df['CH2']), label='CH1-CH2')
-# plt.plot(df['ts'], abs(df['CH3']), label='CH1-CH3')
-# plt.plot(df['ts'], abs(df['CH4']), label='CH1-CH4')
 
 plt.plot(df['ts'], df['CH2'], label='CH1-CH2')
-plt.plot(df['ts'], df['CH3'], label='CH1-CH3')
-plt.plot(df['ts'], df['CH4'], label='CH1-CH4')
+plt.plot(df['ts'], df['CH3']-df['CH4'], label='CH3-CH4')
          
 plt.xlabel("Timestamp")
 plt.ylabel("Phase (deg)")
-plt.title("Phase differences relative to scope channel 1")
+plt.title("Phase differences (outputs same splitter)")
 plt.legend()
 plt.xticks(rotation=45)
 plt.tight_layout()
 
-# plt.savefig(parent_path + "/scope_phases.png")
 
-plt.show()
+plt.savefig(parent_path + "/scope_phases_same_plitter.png")
+
+# plt.show()
