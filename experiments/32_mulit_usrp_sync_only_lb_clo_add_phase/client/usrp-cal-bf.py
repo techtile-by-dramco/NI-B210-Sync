f2b3b8e4c5789a3784c766a8d1b25239a4d92241
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2025-11-18 13:31:02 +0100

    update

--- experiments/32_mulit_usrp_sync_only_lb_clo_add_phase/client/usrp-cal-bf.py
+++ experiments/32_mulit_usrp_sync_only_lb_clo_add_phase/client/usrp-cal-bf.py
@@ -1028,16 +1028,16 @@ def main():
         # STEP 4: Add additional phase to ensure right measurement with the scope
         # -------------------------------------------------------------------------
         phi_offset = 0
-        with open(os.path.join(os.path.dirname(__file__), "config-phase-offsets.yml"), "r") as phases_yaml:
-            try:
-                phases_dict = yaml.safe_load(phases_yaml)
-                if HOSTNAME in phases_dict.keys():
-                    phi_cable = phases_dict[HOSTNAME]
-                    logger.debug(f"Applying phase correction: {phi_offset}")
-                else:
-                    logger.error("Phase offset not found in config-phase-offsets.yml")
-            except yaml.YAMLError as exc:
-                print(exc)
+        # with open(os.path.join(os.path.dirname(__file__), "config-phase-offsets.yml"), "r") as phases_yaml:
+        #     try:
+        #         phases_dict = yaml.safe_load(phases_yaml)
+        #         if HOSTNAME in phases_dict.keys():
+        #             phi_cable = phases_dict[HOSTNAME]
+        #             logger.debug(f"Applying phase correction: {phi_offset}")
+        #         else:
+        #             logger.error("Phase offset not found in config-phase-offsets.yml")
+        #     except yaml.YAMLError as exc:
+        #         print(exc)
 
         # -------------------------------------------------------------------------
         # STEP 5: Benchmark without phase-aligned beamforming
